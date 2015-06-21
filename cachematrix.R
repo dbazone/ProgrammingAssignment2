e6137b86558f35b512ddee97f04825b167165985
Vijay <dbazone@gmail.com>
2015-06-21 18:27:12 +0530

    this is a cache function for inversing the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverted_matrix <- NULL
+	
+	
+	set <- function(new_matrix) {
+		x <<- new_matrix
+		inverted_matrix <<- NULL
+	}
+	
+	get <- function() x
+	
+	## setting the value in cache
+	set_invertible_matrix <- function(z)  inverted_matrix <<- z
+	
+	## getting the value from cache
+	get_invertible_matrix <- function () inverted_matrix
+	
+	list(set = set, get = get,
+		set_invertible_matrix = set_invertible_matrix,
+		get_invertible_matrix = get_invertible_matrix)
+		
 }
 
 
@@ -12,4 +31,23 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		
+		##try to get the matrix from cache first
+		inv_matrix<-x$get_invertible_matrix()
+		
+		##if the value is available in cache then return cached matrix
+		if (!is.null(inv_matrix)){
+			message("getting cached data")
+			return (inv_matrix)
+		}
+		
+		##invert the matrix for the first time
+		data <-x$get()
+		inv_matrix <- solve(data)
+		
+		##setting the inverted matrix in cache
+		x$set_invertible_matrix(inv_matrix)
+		
+		inv_matrix
+		
 }
